342c73ab8f0a20c97d8eab4b746fdc8c12b72ddb
M-axie <136266635+M-axie@users.noreply.github.com>
2024-04-01 16:08:39 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+{
+        "name": "Evil-md",
+        "description": "Evil-md created by M☆XIE",
+  "logo": "https://telegra.ph/file/ca7e5b5526e5cdeb9c8a0.jpg",
+        "keywords": [],
+        "stack": "container",
+        "env":{
+       "HEROKU_API": {
+            "description": "For updating bot and setting vars remotely",
+            "required": true,
+            "value":""
+        },
+        "APP_NAME": {
+            "description": "Exact name which you gave at the top",
+            "required": true,
+            "value":""
+        },
+  "OWNER": {
+    "description": "Enter one number that will be receiving notifications when InfinityAI Starts. .",
+     "required": true,
+    "value": "."
+  },
+"PREFIX": {
+    "description": "Enter your desired prefix for bot.",
+     "required": true,
+    "value": "."
+  },
+"STICKER_PACKNAME": {
+    "description": "Enter packName for your sticker Pack.",
+     "required": false,
+    "value": "Its Me😅"
+  },
+"STICKER_AUTHOR": {
+    "description": "Enter AuthorName for your sticker Pack.",
+     "required": false,
+    "value": "Ma_Xie"
+  },
+  "AUTOBIO": {
+    "description": "If you want your about to auto update.",
+    "value": "TRUE"
+  },
+  "AUTOVIEW_STATUS": {
+    "description": "If you want to autoview status updates.",
+    "value": "TRUE"
+  },
+  "AUTOREAD": {
+    "description": "If you want to blue tick all messages sent to you.",
+    "value": "FALSE"
+  }
+}
+}
